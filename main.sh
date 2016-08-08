12fe5e3da2c480be541686ebb4fba8e0962a0a14
alex-thorne <alex.thorne@gmail.com>
2016-08-08 17:15:43 +0200

    So I found out that it's a problem to 'fc -lnr' from a shell script... working on reading the *correct* histfile via awk (-n2|head -n1)..or something.. also: this project sucks and serves no purpose. I'm only going to finish because I'm stupid and stubborn.

--- main.sh
+++ main.sh
@@ -10,99 +10,124 @@ NORMAL=$(tput sgr0)
 
 default_config() {
   logbook="${HOME}/.shlogbook"
+  myhost=$(hostname)
 }
 
 _day="$(date +%F)"
 _daytime="$(date +%T)"
 TIMESTAMP="$_day at $_daytime" #TODO there must be a better way...
+VERBOSE_STAMP=""
 
 #resolve shlogbook home
-shlog_path="${BASH_SOURCE[0]}"
-while [ -h "$shlog_path" ]; do 
-  TARGET="$(readlink "$shlog_path")"
-  if [[ $TARGET == /* ]]; then 
-    shlog_path="$TARGET"
-  else
-    shlog_home="$( shlog_homename "$shlog_path" )"
-    shlog_path="$shlog_home/$TARGET" 
-  fi
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do 
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" 
 done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-
-echo "$shlog_home" 
-
-
-
-
-config="$shlog_home/.config"
-
-if [ -e "$config" ]; then
+config="$DIR/.config"
+if [[ -e "$config" ]]; then
   source $config
 else 
   default_config
 fi
 
-
+#TODO improve history file resolution
+if [[ ${SHELL} == *"zsh"* ]]; then
+  hist_file="${HOME}/.zsh_history"
+elif [[ ${SHELL} == *"bash"* ]]; then
+  hist_file="${HOME}/.bash_history"
+else
+  echo "please configure history file"
+  exit -1
+fi
 
 ## main ##
 
 get_last() {
-  lastcommand=$(fc -lnr -1 | sed '1s/^[[:space:]]*//')|mktemp /tmp/sb.lastcommand.XXXXX
-  echo "$lastcommand" 
-  #TODO DEBUG - remove echo 
+  lst_cmd=$(tail -n2 $hist_file | head -n1 | mktemp /tmp/sb.lastcommand.XXX)
+  #lst_cmd=$(fc -lnr -1|sed '1s/^[[:space:]]*//'|mktemp /tmp/sb.lastcommand.XXX)
 }
 
-display_last() {
-  echo "$lastcommand"
-}
-
-log_last() {
-  if [ -z "$lastcommand" ]; then
-    lastcommand=$(fc -lnr -1 | sed '1s/^[[:space:]]*//')|mktemp /tmp/sb.lastcommand.XXXXX
-  else
-    echo -e "${RED}err getting last command!${NORMAL}"
-    exit -1
-  fi
-  if [ $2 -eq 1 ]; then
-    read commit_message
-    if [ -z "$commit_message" -a "$commit_message" = " " ]; then
-      echo "<msg> missing or not well-formed, invoke without --message -m for no <msg>"
+main() {
+  get_last
+  #	echo -e "\n\n\n $lst_cmd \n\n\n"
+  if [[ commit_message -eq 1 ]]; then
+    read cmmt_msg
+    if [[ -z "$cmmt_msg" ]]; then #FIXME this still allows for blank ie " " message?
+      echo "<msg> missing or misformed, use without -m|--message to skip"
       exit -1
     fi
   fi
-  echo -e "\n----- $TIMESTAMP -----">>"$logbook"
-  echo -e "\n$lastcommand">>"$logbook"
-  if [ $2 -eq 1 ]; then
-    echo -e "\n$commit_message">>"$logbook"
+  if [[ verbose_log -eq 1 ]]; then
+    verbose_entry="$(date) $USER $myhost $SESSION $SHELL" 
+    echo "\n--- $verbose_entry ---">>$logbook
+  else
+    echo -e "\n----- $TIMESTAMP -----">>"$logbook"
+  fi	
+  echo -e "\n$lst_cmd">>"$logbook"
+  if [[ $commit_message -eq 1 ]]; then
+    echo -e "\n$cmmt_msg">>"$logbook"
   fi
-  echo -e "\n$lastcommand! ${GREEN}added to shlogbook!${NORMAL}"
+  if [[ verbose_out -eq 1 ]]; then
+    if [[ verbose_log -eq 1 ]]; then
+      echo -e "\n$verbose_log"
+    fi		
+    echo -e "\n$lst_cmd! \n${GREEN}added to shlogbook!${NORMAL}"
+  fi	
   exit 0
 }
 
 ## end main ##
 
 ## shlogbook accepts arguments! ##
-while [ $# -gt 0 ]; do
+#TODO alphabetize and document
+while [[ $# -gt 0 ]]; do
   case "$1" in
-    -g|--display)
-      display_last=1
+    -v|--verbose)
+      verbose_out=1 #Generously shows user an output
+      shift
       ;;
-    -l|--log)
-      log_last=1
+    -r|--range)  #TODO (support selection from fc -lnr -(>1)items)
+      shift
+      ;;
+    -t|--test) #TODO runs 'dry-run' with option to log after displaying output
+      dry_run=1
+      shift
       ;;
     -m|--message)
       commit_message=1
+      shift
+      ;;
+    -V|--verbose-logging)
+      verbose_log=1 #Generously shows user an output
       ;;
     -c|--config) #TODO allow for in-line shlogbook config 
       CONFFILE="$1"
       shift
-      if [ ! -f $CONFFILE ]; then
+      if [[ ! -f $CONFFILE ]]; then
         echo "${RED}Error reading shlogbook config file${NORMAL}"
         exit -1  # TODO error handling
       fi
+      ;;
+    -h|--help)
+      cat "$DIR/help.txt"
+      ;;
+    --default) # End of all options
+      shift
+      ;;
+    -*)
+      echo "Error: Unknown option: $1" >&2
+      exit 1
+      ;;
+    *)
+      echo -e "Invalid option ("shlogbook --help" for help)"
+      exit 0
       ;;
   esac
   shift  # Check next set of parameters.
 done
 
-exit 0
+main
